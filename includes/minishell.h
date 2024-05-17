cebcbd1eae21e4d0137412ac0b7b7aaad56c9174
mstrauss <matthias.strauss@proton.me>
2024-05-17 15:12:01 +0200

    added new functions to header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 16:00:02 by mstrauss          #+#    #+#             */
-/*   Updated: 2024/05/17 14:53:22 by mstrauss         ###   ########.fr       */
+/*   Updated: 2024/05/17 15:11:31 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 # define MINISHELL_H
 
 # ifndef LIBFT_H
-#  include "./includes/libft/libft.h"
-#  include "enviroment/env.h"
-#  include "signal/sig_handl.h"
-#  include <unistd.h>
+#  include "libft.h"
 # endif
-# include <errno.h>
+# include "enviroment/env.h"   // koennen wir die yeeten?
+# include "signal/sig_handl.h" // bitte ALLES in minishell.h, ist uebersichtlich
+# include <errno.h>            // genug und ich brauche nicht 10 files oeffnen
 # include <signal.h>
+# include <stdarg.h>
 # include <stdbool.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 /* -------------------------------------------------------------------------- */
 /*                                   STRUCTS                                  */
@@ -30,7 +31,6 @@
 typedef struct s_token
 {
 	char	*token;
-
 }			t_token;
 
 /* -------------------------------------------------------------------------- */
@@ -42,13 +42,18 @@ typedef struct s_token
 /* -------------------------------- Get Input ------------------------------- */
 
 /* ---------------------------- Memory Management --------------------------- */
+void		ft_free(void **ptr);
+void		*ft_malloc(void **ptr, int size);
 
 /* -------------------------------- Execution ------------------------------- */
 
 /* ------------------------- Join Commands and ARGs ------------------------- */
 
 /* ------------------------------ Tokenization ------------------------------ */
+char		*ft_strtok(char *str, const char *delimiters);
 
 /* --------------------------------- Parsing -------------------------------- */
+size_t		ft_strspn(const char *str1, const char *str2);
+char		*ft_strjoinall(int count, bool free, ...);
 
 #endif
